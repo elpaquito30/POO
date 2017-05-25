7a6914d609da19149ae47bbcd496193447cf9d6f
Nando <nando-fmap@hotmail.com>
2017-05-25 19:55:30 +0100

    alteracoes

--- Viatura.java
+++ Viatura.java
@@ -10,21 +10,24 @@ public abstract class Viatura
     private double velocidade;
     private double custo;
     private Posicao p;
+    private Motorista m;
     
-        public Viatura(double velocidade, double custo, Posicao p){
+        public Viatura(double velocidade, double custo, Posicao p, Motorista m){
             this.velocidade = velocidade;
             this.custo = custo;
             this.p = p.clone();
+            this.m = m.clone();
         }
         
         public Viatura(){
-            this(0,0, new Posicao());
+            this(0,0, new Posicao(), new Motorista());
         }
         
         public Viatura(Viatura u){
             this.velocidade = u.getVelocidade();
             this.custo = u.getCusto();
             this.p = u.getP();
+            this.m = u.getMotorista();
         }
         
         public double getVelocidade(){
@@ -39,6 +42,10 @@ public abstract class Viatura
             return p;
         }
         
+        public Motorista getMotorista(){
+            return m;
+        }
+        
         public void setVelocidade(double velocidade){
             this.velocidade = velocidade;
         }
@@ -51,6 +58,10 @@ public abstract class Viatura
             this.p = p;
         }
         
+        public void setM(Motorista m){
+            this.m = m;
+        }
+        
         public boolean equals(Object o){
             if(this == o)
                 return true;
@@ -59,7 +70,8 @@ public abstract class Viatura
             Viatura m = (Viatura) o;
                 return (this.velocidade==(m.getVelocidade()) && 
                this.custo==(m.getCusto()) && 
-               this.p.equals(m.getP()));
+               this.p.equals(m.getP()) &&
+               this.m.equals(m.getM()));
         }
         
         public String toString() {
@@ -67,6 +79,7 @@ public abstract class Viatura
             sb.append("Velocidade: ").append(velocidade).append("\n");
             sb.append("Custo: ").append(custo).append("\n");
             sb.append("Posicao: ").append(p).append("\n");
+            sb.append("Motorista: ").append(m).append("\n");
             return sb.toString();
         }
         
